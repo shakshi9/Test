dc927e56fc6e261c5c1637c81ebeb8126c7702fc
Santhos <santhoshsunthar@gmail.com>
2023-01-16 18:46:45 +0530

    changes are added

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 $server = "localhost";
 $user = "root";
-$pass = "toor";
+$pass = "";
 $db = "bookstall";
 try{
     $con = new PDO("mysql:host = $server;dbname=$db;charset=UTF8",$user,$pass);
